7a999dd0081c26cd12aea7339228e1e8da0a7af4
Sam <amazing_sam@outlook.com>
2020-02-02 11:27:33 +0800

    第一题两数之和

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -11,13 +11,23 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    nums.forEach((e, i) => {
-        nums.forEach((m, n) => {
-            if (e + m === 9) {
-                return [i, n]
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
             }
-        })
-    })
+        }
+    }
+    // nums.forEach((e, i) => {
+    //     nums.forEach((m, n) => {
+    //         if (e + m === target) {
+    //             return [i, n]
+    //         }
+    //     })
+    // })
+    // 除了抛出异常以外，没有办法中止或跳出 forEach() 循环。
+    // 如果你需要中止或跳出循环，forEach() 方法不是应当使用的工具。
+    // 因此上述代码没用。
 };
 // @lc code=end
 
