77d2074124fbb0a24f6d1b893f74b2af6c4edaea
SamLiu <amazing_sam@outlook.com>
2020-02-28 00:28:24 +0800

    26. 删除排序数组中的重复项

--- 26.删除排序数组中的重复项.js
+++ 26.删除排序数组中的重复项.js
@@ -9,14 +9,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-//  i + 1 会遇到两个元素相同却没有办法的问题
-//  为 i 会遇到全都给删除的情况
 var removeDuplicates = function(nums) {
     let temp = {}
-    for (let i = nums.length; i > 0; i--) {
-        temp[nums[i]] = true
-        if (temp[nums[i - 1]]) {
-            nums.splice(i - 1, 1)
+    for (let i = 0; i < nums.length;) {
+        if(temp[nums[i]]) {
+            nums.splice(i, 1)
+        } else {
+            temp[nums[i]] = true
+            i++
         }
     }
     return nums.length
